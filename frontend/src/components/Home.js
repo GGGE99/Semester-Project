5cd3d6e35d297a0355713f1da4d2fc0997541ce1
Marcus Ravn Jensen <marcusravn1996'gmail.com>
2020-11-25 14:04:09 +0100

    added bootstrap

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,39 +1,49 @@
 import React, { useState, useEffect } from "react";
 import { allCoinsURL } from "../utils/settings";
-import { makeOptions, handleHttpErrors } from "../utils/fetchUtils"
-import Table from 'react-bootstrap/Table';
-
-
-
-
+import { makeOptions, handleHttpErrors } from "../utils/fetchUtils";
+import Table from "react-bootstrap/Table";
+import { Col, Row } from "react-bootstrap";
 
 export default function AllCoins() {
-    const [coin, setCoin] = useState([])
-    useEffect(() => {
-        const options = makeOptions("GET", true);
-        fetch(allCoinsURL, options)
-            .then(handleHttpErrors)
-            .then((data) => {
-                console.log(data.all)
-                setCoin([...data])
+  const [coin, setCoin] = useState([]);
+  useEffect(() => {
+    const options = makeOptions("GET", true);
+    fetch(allCoinsURL, options)
+      .then(handleHttpErrors)
+      .then((data) => {
+        console.log(data.all);
+        setCoin([...data]);
+      });
+  }, []);
 
-
-            })
-    }, [])
-
-
-    return (
-        <div>
-          <Table>
-                <thead><tr><td>Name</td><td>Price</td><td>Last Updated</td><td>volume</td></tr></thead>
-                <tbody>{coin.map((p) => {
-                    return (
-                    <tr key={p.name}><td>{p.name}</td><td>{p.price}</td><td>{p.lastUpdated}</td><td>{p.volume}</td></tr>
-                    )
-                })}</tbody>
-            </Table>
-
-        </div>
-    )
+  return (
+    <Row>
+      <Col></Col>
+      <Col>
+        <Table>
+          <thead>
+            <tr>
+              <td>Name</td>
+              <td>Price</td>
+              <td>Last Updated</td>
+              <td>Volume</td>
+            </tr>
+          </thead>
+          <tbody>
+            {coin.map((p) => {
+              return (
+                <tr key={p.name}>
+                  <td>{p.name}</td>
+                  <td>{p.price}</td>
+                  <td>{p.lastUpdated}</td>
+                  <td>{p.volume}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      </Col>
+      <Col></Col>
+    </Row>
+  );
 }
-            
