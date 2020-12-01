c9ccea66b4719db39c9073ec677adfe0cfbe8cf3
Marcus Ravn Jensen <marcusravn1996'gmail.com>
2020-12-01 14:57:38 +0100

    sort funktion m styling

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { allCoinsURL } from "../utils/settings";
 import { makeOptions, handleHttpErrors } from "../utils/fetchUtils";
 import Table from "react-bootstrap/Table";
-import { Col, Row } from "react-bootstrap";
+import { Button, Col, Row } from "react-bootstrap";
 import Currency from "./Currency";
 
 export default function AllCoins() {
@@ -31,19 +31,34 @@ export default function AllCoins() {
       });
   }, []);
 
+   function compare(a,b){
+      if(a < b) return 1
+      else if(a > b) return -1
+      return 0
+  }
+
+  function sort(sortValue){
+    if(sortValue === "price")setCoin([...coin.sort((a,b) => compare(a.price, b.price))])
+    else if(sortValue === "volume")setCoin([...coin.sort((a,b) => compare(a.volume, b.volume))])
+    else if(sortValue === "name")setCoin([...coin.sort((a,b) => compare(a.name.toLowerCase(), b.name.toLowerCase()))])
+    
+  }
+
   return (
     <Row>
       <Col>
         <Currency currency={currency} setCurrency={setCurrency} />
       </Col>
       <Col>
+       
         <Table>
           <thead>
             <tr>
-              <td>Name</td>
-              <td>Price</td>
+             
+              <td onClick={()=>sort("name")}><Button className="btn btn-warning">Name</Button></td>
+              <td onClick={()=>sort("price")}><Button className="btn btn-warning">Price</Button></td>
               <td>Last Updated</td>
-              <td>Volume</td>
+              <td onClick={()=>sort("volume")}><Button className="btn btn-warning">Volume</Button></td>
             </tr>
           </thead>
           <tbody>
