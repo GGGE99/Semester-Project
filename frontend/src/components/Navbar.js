2fb950041a7bb1b7ebb657cb186b56f7843c08d3
Mads Kristensen <madsarke@gmail.com>
2020-12-04 13:30:41 +0100

    logout sender dig til forsiden

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Navbar, Nav } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
 
 function NavbarShow({ user, logout }) {
+  const history = useHistory();
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Link to="/" className="navbar-brand">
@@ -24,33 +26,38 @@ function NavbarShow({ user, logout }) {
               Profile
             </Link>
           ) : (
-            <></>
-          )}
+              <></>
+            )}
 
           {localStorage.getItem("jwtToken") ? (
             <Link to="/settings" className="nav-link">
               Settings
             </Link>
           ) : (
-            <></>
-          )}
+              <></>
+            )}
         </Nav>
         <Nav>
           {localStorage.getItem("jwtToken") ? (
             <>
               <h5 className="mr-2 text-light">{user.username}</h5>
-              <button className="btn btn-danger" onClick={logout}>
+              <button className="btn btn-danger" onClick={() => {
+                let path = ""
+                logout()
+                history.push(path)
+              }
+              }>
                 Logout
               </button>
             </>
           ) : (
-            <Link to="/signin">
-              <button className="btn btn-primary">Sign In</button>
-            </Link>
-          )}
+              <Link to="/signin">
+                <button className="btn btn-primary">Sign In</button>
+              </Link>
+            )}
         </Nav>
       </Navbar.Collapse>
-    </Navbar>
+    </Navbar >
   );
 }
 export default NavbarShow;
