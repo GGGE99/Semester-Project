5d356d9323bf1e2ad69a70117d80384b58456a5e
Marcus Ravn Jensen <marcusravn1996'gmail.com>
2020-12-07 14:39:42 +0100

    settings

--- frontend/src/utils/settings.js
+++ frontend/src/utils/settings.js
@@ -1,5 +1,5 @@
-//const baseURL = "https://marcge.dk/tomcat/SemesterProject"
-const baseURL = "http://localhost:8080/jpareststarter"
+const baseURL = "https://marcge.dk/tomcat/SemesterProject"
+//const baseURL = "http://localhost:8080/jpareststarter"
 
 
 export const loginURL = baseURL;
